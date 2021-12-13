fcc7afe0967eebdc11db69b547ac9a50a654569f
kshelp <kshelp@naver.com>
2021-12-13 11:48:56 +0900

    .

--- adminLTELab/polls/urls.py
+++ adminLTELab/polls/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from django.urls.resolvers import URLPattern
-from . import views
+from polls import views
+
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('profile/', views.profile, name='profile'),
+    # /polls/
+    path('', views.IndexView.as_view(), name='index'),
+
+    # /polls/99/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # /polls/99/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    # /polls/99/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
